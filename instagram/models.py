19fc19ee67f4a7e4cc54f292ae81323fdae9dff3
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-10 23:16:20 +0300

    created model Profile

--- instagram/models.py
+++ instagram/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    profile_photo = models.ImageField(upload_to ="profile/")
+    bio = models.TextField()
