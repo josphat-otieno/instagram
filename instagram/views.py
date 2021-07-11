2b6128e065b08a2fc39493ca8988960a3843ae1a
josphat-otieno <josephat.otieno@students.moringaschool.com>
2021-07-11 12:37:35 +0300

    create url patterns and view functions

--- instagram/views.py
+++ instagram/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse, Http404
 
 # Create your views here.
+def index(request):
+    name = 'instagram app'
+
+    return render(request, 'index.html', {"name":name})
